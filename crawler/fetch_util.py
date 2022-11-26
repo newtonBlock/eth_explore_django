485bd9ba20578dbc57b7908871becb6e9750983b
Wang Xuetao <xuetao_wang@veragroup.com>
2022-11-26 11:06:31 +0800

    get the quicknote api

---
+++ crawler/fetch_util.py
@@ -0,0 +1,41 @@
+import os
+
+def refresh_logger(filename):
+    """Remove old logs and create new ones."""
+    if os.path.isfile(filename):
+        try:
+            os.remove(filename)
+        except Exception:
+            pass
+    open(filename, 'a').close()
+
+    
+
+def decodeBlock(block):
+    """
+    Decode various pieces of info for a block and return the parsed data
+    """
+
+    try:
+        b = block
+
+        new_block = {
+            "number": b["number"],
+            "timestamp": b["timestamp"],
+            "transactions": []
+        }
+
+        #Filter and decode each tx
+        #Value, gas, and gasPrice are all converted tp ether
+        for t in b["transactions"]:
+            new_t = {
+                "from": t["from"],
+                "to": t["to"],
+                "value": t["value"]/1000000000000000000.,
+                "data": t["input"]
+            }
+            new_block["transactions"].append(new_t)
+        return new_block
+
+    except:
+        return None
--- crawler/fetcher.py
+++ crawler/fetcher.py
@@ -1,4 +1,46 @@
+import os
+import sys
+import logging
+
+from web3 import Web3, HTTPProvider
+
+import fetch_util
+
+DIR = sys.path.append(os.path.realpath(os.path.dirname(__file__)))
+LOGFIL = "crawler.log"
+#LOGFIL = "{}/{}".format(DIR, LOGFIL)
+fetch_util.refresh_logger(LOGFIL)
+logging.basicConfig(filename=LOGFIL, level=logging.DEBUG)
+logging.getLogger("urllib3").setLevel(logging.WARNING)
 
 class Fetcher(object):
-    def __init__(self) -> None:
+
+    def __init__(
+        self,
+        start=True,
+        host="",
+        delay=0.0001) -> None:
+
+        logging.debug("Starting Crawler")
+        self.url = ""
+        self.headers = {"content-type": "application/json"}
+
+        self.delay = delay
+        self.w3 = Web3(HTTPProvider('https://fabled-bitter-pallet.discover.quiknode.pro/8dc4d68113f456c734f016b4f4f197a657de3b48/'))
+
+        if start:
+            self.run()
+
+    def getBlockByNumber(self, n = 'latest'):
+        """Get a specific block from the chain and filer the data"""
+        data = self.w3.eth.get_block(n)
+        logging.info(data)
+        block = fetch_util.decodeBlock(data)
+
+    def run(self):
         pass
+
+if __name__ == "__main__":
+
+    f = Fetcher()
+    f.getBlockByNumber()
